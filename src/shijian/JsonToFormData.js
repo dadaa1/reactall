2d4bb7e86c159419146e6a0fcef1d0890ca6a810
dadaa1 <1692972759@qq.com>
2018-02-27 15:24:04 +0800

    0.0

--- src/shijian/JsonToFormData.js
+++ src/shijian/JsonToFormData.js
@@ -1,4 +1,4 @@
-function JsonToFormData(json){//暂时没有考虑文件的情况
+function JsonToFormData(json){
 	let formdata=new FormData();
 	for(let key in json){
 		formdata.append(key,json[key]);
@@ -15,27 +15,27 @@ function formDataToJson(formdata){
    return obj
 }
 
-// let accc=JsonToFormData({
-// 	a:1,
-// 	b:2,
-// 	c:3
-// })
+// // let accc=JsonToFormData({
+// // 	a:1,
+// // 	b:2,
+// // 	c:3
+// // })
 
-// let bccc=formDataToJson(accc);
+// // let bccc=formDataToJson(accc);
 
-// console.log('我是bccc',bccc)
+// // console.log('我是bccc',bccc)
 
-// console.log(accc);
-// for (var [key, value] of accc.entries()) { 
-//   console.log(key, value);
+// // console.log(accc);
+// // for (var [key, value] of accc.entries()) { 
+// //   console.log(key, value);
+// // }
+// function aa(){
+// 	let f=new FormData(document.getElementById('form'));
+// 	let f1=formDataToJson(f);
+// 	console.log(f1);
+// 	let f2=JsonToFormData(f1);
+// 	console.log(f2);
+// 	for (var [key, value] of f2.entries()) { 
+// 	  console.log('iiiiii',key, value);
+// 	}
 // }
-function aa(){
-	let f=new FormData(document.getElementById('form'));
-	let f1=formDataToJson(f);
-	console.log(f1);
-	let f2=JsonToFormData(f1);
-	console.log(f2);
-	for (var [key, value] of f2.entries()) { 
-	  console.log('iiiiii',key, value);
-	}
-}
