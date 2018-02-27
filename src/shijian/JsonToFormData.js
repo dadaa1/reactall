973c354a31e5687fa19cb9e5e1abf76e47ebab1d
dadaa1 <1692972759@qq.com>
2018-02-27 15:23:22 +0800

    阿达

--- src/shijian/JsonToFormData.js
+++ src/shijian/JsonToFormData.js
@@ -9,22 +9,33 @@ function formDataToJson(formdata){
     let arr=formdata.entries();
     let obj={};
     for( let [key,value] of arr){
+    	console.log(key,value);
       obj[key]=value;
     }
    return obj
 }
 
-let accc=JsonToFormData({
-	a:1,
-	b:2,
-	c:3
-})
+// let accc=JsonToFormData({
+// 	a:1,
+// 	b:2,
+// 	c:3
+// })
 
-let bccc=formDataToJson(accc);
+// let bccc=formDataToJson(accc);
 
-console.log('我是bccc',bccc)
+// console.log('我是bccc',bccc)
 
 // console.log(accc);
 // for (var [key, value] of accc.entries()) { 
 //   console.log(key, value);
 // }
+function aa(){
+	let f=new FormData(document.getElementById('form'));
+	let f1=formDataToJson(f);
+	console.log(f1);
+	let f2=JsonToFormData(f1);
+	console.log(f2);
+	for (var [key, value] of f2.entries()) { 
+	  console.log('iiiiii',key, value);
+	}
+}
